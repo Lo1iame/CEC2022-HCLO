9053ec40a88eab7d092fbea8816ccdec567c40a0
loriame <1007574921@qq.com>
2022-02-23 15:30:44 +0800

    test

--- js/statistics.js
+++ js/statistics.js
@@ -1,1 +1,10 @@
 console.log("test");
+
+axios
+  .get("https://v1.hitokoto.cn?c=a")
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
