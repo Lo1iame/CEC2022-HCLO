436f2de61d6b6a5bbf9ccbae4e46a9e37b3c5ffa
loriame <1007574921@qq.com>
2022-02-24 10:43:29 +0800

    test

--- js/statistics.js
+++ js/statistics.js
@@ -1,4 +1,11 @@
-axios.get("http://49.232.44.253:8080/quantity/viewer");
+axios
+  .get("http://49.232.44.253:8080/quantity/viewer")
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 window.downloadFile = function (src) {
   console.log(src);
