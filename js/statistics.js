d3967370bc2bba782348231463a02705e4f61d2f
loriame <1007574921@qq.com>
2022-02-24 11:31:54 +0800

    test

--- js/statistics.js
+++ js/statistics.js
@@ -7,6 +7,38 @@ axios
     console.log(error);
   });
 
-window.downloadFile = function (src) {
-  console.log(src);
+window.downloadFile = function (dataset) {
+  console.log(dataset);
+  const modal = document.getElementById("modal");
+  modal.style.display = "flex";
+  window.fileDataset = dataset;
+};
+
+window.downloadSubmit = function () {
+  const filename = window.fileDataset.filename;
+  const name = document.getElementById("Name").value;
+  const institution = document.getElementById("Institution").value;
+  const email = document.getElementById("Email").value;
+
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("institution", institution);
+  formData.append("email", email);
+  formData.append("filename", filename);
+
+  axios
+    .post("http://49.232.44.253:8080/quantity/viewer", formData)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  modal.style.display = "none";
+  window.open(window.fileSrc);
+};
+
+window.closeModal = function () {
+  modal.style.display = "none";
 };
